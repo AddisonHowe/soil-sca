77a42d1c3906193299bf20192ebb77fdea8bdbff
Addison Howe <addi.howe@gmail.com>
2025-10-29 15:51:26 -0500

    added msa 57

---
+++ data/K00370/msa_scripts/alnscript_MSA_57.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+datdir=data/K00370
+seq_fname=soil_seqs_57.fasta
+out_fname=MSA_57
+
+nthreads=16
+
+infile=${datdir}/seqs/${seq_fname}
+outdir=${datdir}/msas
+guidetreedir=${datdir}/guidetrees
+outfile=${outdir}/${out_fname}.aln-fasta
+
+echo "Aligning sequences in file ${infile}"
+echo "Saving results to file ${outfile}"
+
+mkdir -p $outdir
+mkdir -p $guidetreedir
+
+clustalo --infile ${infile} --outfile ${outfile} \
+    --seqtype protein --outfmt fa --output-order tree-order --verbose \
+    --guidetree-out ${guidetreedir}/${out_fname}.dnd  \
+    --threads ${nthreads}
