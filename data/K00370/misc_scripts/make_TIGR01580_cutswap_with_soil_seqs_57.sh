ae5a2bedc34a5a40ff0e90357f7a3a1633284889
Addison Howe <addi.howe@gmail.com>
2025-11-07 21:33:14 -0600

    small name change

--- data/K00370/misc_scripts/make_TIGR01580_cutswap_with_soil_seqs_57.sh
+++ data/K00370/misc_scripts/make_TIGR01580_cutswap_with_soil_seqs_57.sh
@@ -3,5 +3,5 @@
 python scripts/cutswap_sequences.py \
     -m data/K00370/msas/MSA_TIGR01580_with_soil_seqs_57.aln-fasta \
     -r results/TIGR01580_with_soil_seqs_57/sca_results \
-    -o data/K00370/seqs/cutswap_TIGR01580_with_soil_seqs_57.fasta \
+    -o data/K00370/seqs/TIGR01580_cutswap_with_soil_seqs_57.fasta \
     --percentile 50
