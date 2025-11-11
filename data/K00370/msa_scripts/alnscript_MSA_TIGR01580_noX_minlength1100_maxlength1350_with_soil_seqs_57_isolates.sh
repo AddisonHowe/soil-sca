9279767feeee138c3ab0916ede87f85a5cbee20f
Addison Howe <addi.howe@gmail.com>
2025-11-11 15:24:51 -0600

    added isolates fasta and script

---
+++ data/K00370/msa_scripts/alnscript_MSA_TIGR01580_noX_minlength1100_maxlength1350_with_soil_seqs_57_isolates.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+datdir=data/K00370
+seq_fname=TIGR01580_noX_minlength1100_maxlength1350_with_soil_seqs_57_isolates.fasta
+out_fname=MSA_TIGR01580_noX_minlength1100_maxlength1350_with_soil_seqs_57_isolates
+
+nthreads=16
+
+infile=${datdir}/seqs/${seq_fname}
+outdir=${datdir}/msas
+guidetreedir=${datdir}/guidetrees
+outfile=${outdir}/${out_fname}.aln-fasta
+
+echo "Aligning sequences in file ${infile}"
+echo "Saving results to file ${outfile}"
+
+mkdir -p $outdir
+mkdir -p $guidetreedir
+
+clustalo --infile ${infile} --outfile ${outfile} \
+    --seqtype protein --outfmt fa --output-order tree-order --verbose \
+    --guidetree-out ${guidetreedir}/${out_fname}.dnd  \
+    --threads ${nthreads}
