cc52881031a216a127b1b395619d903b257b1e54
Addison Howe (from Quest) <addi.howe@gmail.com>
2025-11-17 18:58:15 -0600

    added data

--- data/K00370/msa_scripts/alnscript_MSA_cutswap_TIGR01580_with_soil_seqs_57.sh
+++ data/K00370/msa_scripts/alnscript_MSA_cutswap_TIGR01580_with_soil_seqs_57.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 datdir=data/K00370
-seq_fname=cutswap_TIGR01580_with_soil_seqs_57.fasta
+seq_fname=TIGR01580_cutswap_with_soil_seqs_57.fasta
 out_fname=MSA_TIGR01580_cutswap_with_soil_seqs_57
 
 nthreads=16
